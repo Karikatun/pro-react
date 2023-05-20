579bf5c3035d6faebc7759e22b2dcd08d11bfdc9
Anton Shevchenko <shevchenko-anton2014@yandex.ru>
2023-05-20 21:22:22 +0300

    Add toggle theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import { Route, Routes } from 'react-router-dom'
 
-import './index.scss'
+import './styles/index.scss'
 import { NavLink } from 'react-router-dom'
 import { lazy, Suspense } from 'react'
+import { useTheme } from './theme/useTheme'
 
 const AboutPage = lazy(() => import('./pages/about/AboutPage'))
 const MainPage = lazy(() => import('./pages/main/MainPage'))
 
 const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <div className='app'>
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>Toggle</button>
       <NavLink to='/'>Главная</NavLink>
       <NavLink to='/about'>О сайте</NavLink>
       <Suspense fallback={<h1>Loading...</h1>}>
