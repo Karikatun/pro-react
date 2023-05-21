05b44696ffad8b283ab124bbcc0fbc8e662e6012
Anton Shevchenko <shevchenko-anton2014@yandex.ru>
2023-05-21 15:51:25 +0300

    add classNames function

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import './styles/index.scss'
 import { NavLink } from 'react-router-dom'
 import { lazy, Suspense } from 'react'
 import { useTheme } from './theme/useTheme'
+import { classNames } from './helpers/classNames/classNames'
 
 const AboutPage = lazy(() => import('./pages/about/AboutPage'))
 const MainPage = lazy(() => import('./pages/main/MainPage'))
@@ -12,7 +13,7 @@ const App = () => {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <div className={`app ${theme}`}>
+    <div className={classNames('app', {}, [theme])}>
       <button onClick={toggleTheme}>Toggle</button>
       <NavLink to='/'>Главная</NavLink>
       <NavLink to='/about'>О сайте</NavLink>
